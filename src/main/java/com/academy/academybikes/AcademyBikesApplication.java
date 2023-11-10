b4bfef873e921478d5bc9fa8be7ba348f1b70082
niko kauppinen <marina.jarvinen@hotmail.com>
2023-11-10 17:49:33 +0200

    first commit

---
+++ src/main/java/com/academy/academybikes/AcademyBikesApplication.java
@@ -0,0 +1,13 @@
+package com.academy.academybikes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademyBikesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademyBikesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/academy/academybikes/AcademyBikesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academy.academybikes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademyBikesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
